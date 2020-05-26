fa0404caf5eb3ba194dd298111f45c050cdb1e1c
liyao <904117257@qq.com>
2020-05-26 13:28:15 +0800

    输入一串字符，由上到下，由左到右进行z字形排列，输出排列后的字符串。

---
+++ 字符串的z字形变换.c
@@ -0,0 +1,21 @@
+char * convert(char * s, int numRows)
+{
+    int len=strlen(s);
+    int tab=2*numRows-2;
+    if(len<=numRows || tab==0)
+    return s;
+    char* res=(char *)malloc(sizeof(char *)*(len+1));
+    int num=0;
+    for(int i=0;i<=tab/2;i++)
+    {
+        for(int j=0;j<len;j++)
+        {
+            if((j+tab)%tab==i || (j+tab)%tab==(tab-i))
+            {
+                res[num++]=s[j];
+            }
+        }
+    }
+    res[num]='\0';
+    return res;
+}
